b96d7a16379cb088286b48d2f1e20c46d82a676d
Piero Funes <pierofl2005@gmail.com>
2026-02-13 08:51:42 +0100

    1

--- back/conexion/conexion_bbdd.py
+++ back/conexion/conexion_bbdd.py
@@ -5,16 +5,12 @@ def obtener_conexion():
     try:
         conexion = mysql.connector.connect(
             host='localhost',
-            user='resilio',      # Cambia por tu usuario de MySQL
-            password='Resilio123$',  # Cambia por tu contraseña
-            database='resilio'       # El nombre de tu BBDD
+            user='resilio',      
+            password='Resilio123$',  
+            database='resilio'       
         )
         if conexion.is_connected():
             return conexion
     except Error as e:
         print(f"Error al conectar a MySQL: {e}")
-        return None
-
-# Ejemplo de cómo usarías el cursor en tus funciones:
-# conexion = obtener_conexion()
-# cursor = conexion.cursor(dictionary=True) # dictionary=True devuelve los datos como diccionarios
+        return None 
